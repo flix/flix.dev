3cf490f133cc70a3b79c20fca43015ce4e3672fd
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2018-12-11 14:16:23 -0500

    Added some design principles.

--- src/page/DesignPrinciples.js
+++ src/page/DesignPrinciples.js
@@ -1,35 +1,104 @@
 import React, {Component} from 'react';
-
-import {
-    Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle, Button
-} from 'reactstrap';
-
+import {Card, CardText, CardBody, CardTitle} from 'reactstrap';
 import {Container, Row, Col} from 'reactstrap';
 
+import lipsum from 'lorem-ipsum';
 
 class DesignPrinciples extends Component {
     render() {
         return (
-            <div>
+            <Container>
                 <h1>Design Principles</h1>
 
-                <Container>
-                    <Row>
-                        <Col xs="6">
+                <Row>
+                    <Col xs="6">
 
-                            <Principle name=" Safety first, performance second.">
-                                bla bla bla
-                            </Principle>
-                        </Col>
-                        <Col xs="6">
-                            <Principle name=" Simple is not easy, will not sacrifice a principle for practicality">
-                                bla bla bla
-                            </Principle>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
+                        <Principle name="Safety first, performance second.">
+                            {lipsum({count: 3, units: 'sentences'})}
+                        </Principle>
+
+                        <Principle name="    No order of declarations">
+                            {lipsum({count: 3, units: 'sentences'})}
+                        </Principle>
+
+                        <Principle name=" No coercions">
+                            {lipsum({count: 3, units: 'sentences'})}
+                        </Principle>
+
+                        <Principle name="   No global state.">
+                            {lipsum({count: 3, units: 'sentences'})}
+                        </Principle>
+
+                        <Principle name="     Keyword-based syntax">
+                            {lipsum({count: 3, units: 'sentences'})}
+                        </Principle>
+
+
+                        <Principle name="    Every function is curried">
+                            {lipsum({count: 3, units: 'sentences'})}
+                        </Principle>
+
+                        <Principle name="        Local type inference">
+                            {lipsum({count: 3, units: 'sentences'})}
+                        </Principle>
+
+                        <Principle name="      Uniform function call syntax (UFCS)">
+                            {lipsum({count: 3, units: 'sentences'})}
+                        </Principle>
+
+                        <Principle name="         Consistent syntax for expressions and types">
+                            {lipsum({count: 3, units: 'sentences'})}
+                        </Principle>
+
+                        <Principle name="               Private visibility by default">
+                            {lipsum({count: 3, units: 'sentences'})}
+                        </Principle>
+
+                    </Col>
+                    <Col xs="6">
+                        <Principle name=" Simple is not easy, will not sacrifice a principle for practicality">
+                            {lipsum({count: 3, units: 'sentences'})}
+                        </Principle>
+
+
+                        <Principle name="   Everything is an expression">
+                            {lipsum({count: 3, units: 'sentences'})}
+                        </Principle>
+
+                        <Principle name=" Arbitrary-precision Arithmetic.">
+                            {lipsum({count: 3, units: 'sentences'})}
+                        </Principle>
+
+                        <Principle name="     Illegal states should be unrepresentable.">
+                            {lipsum({count: 3, units: 'sentences'})}
+                        </Principle>
+
+                        <Principle name="    No-nulls.">
+                            {lipsum({count: 3, units: 'sentences'})}
+                        </Principle>
+
+                        <Principle name="          No Initialization code before main">
+                            {lipsum({count: 3, units: 'sentences'})}
+                        </Principle>
+
+                        <Principle name=" All warnings are compile-time errors">
+                            {lipsum({count: 3, units: 'sentences'})}
+                        </Principle>
+
+                        <Principle name="            Human-Readable error messages">
+                            {lipsum({count: 3, units: 'sentences'})}
+                        </Principle>
+
+                        <Principle name="    Patterns must be exhaustive">
+                            {lipsum({count: 3, units: 'sentences'})}
+                        </Principle>
+
+                        <Principle name="    Checked array accesses.">
+                            {lipsum({count: 3, units: 'sentences'})}
+                        </Principle>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
