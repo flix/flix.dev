6d10bba97fcbee12eb96ffe2e07ac4510a5c1013
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2018-12-11 15:36:18 -0500

    Added information about research.

--- src/page/Research.js
+++ src/page/Research.js
@@ -7,24 +7,59 @@ class Research extends Component {
                 <h1>Research</h1>
 
                 <p>
-                    Flix
+                    <strike>
+                        Flix
+                    </strike>
                 </p>
 
-                <ul>
-                    <Paper name="Implicit Parameters for Logic Programming"
-                    />
+                <h3>Peer-Reviewed Conferences and Journals</h3>
 
-                    <Paper name="Safe and Sound Program Analysis with Flix "
-                    />
+                <ul>
+                    <Paper
+                        title="Implicit Parameters for Logic Programming"
+                        authors="Magnus Madsen, Ondřej Lhoták"
+                        venue="PPDP '18"
+                        url="https://dl.acm.org/citation.cfm?id=3236953"/>
 
                     <Paper
-                        name="Tail Call Elimination and Data Representation for Functional Languages on the Java Virtual Machine"
-                    />
+                        title="Safe and Sound Program Analysis with Flix"
+                        authors="Magnus Madsen, Ondřej Lhoták"
+                        venue="ISSTA '18"
+                        url="https://dl.acm.org/citation.cfm?id=3213847"/>
 
-                    <Paper name="From Datalog to Flix: A Declarative Language for Fixed Points on Lattices"
-                    />
+                    <Paper
+                        title="Tail Call Elimination and Data Representation for Functional Languages on the Java Virtual Machine"
+                        authors="Magnus Madsen, Ramin Zarifi, Ondřej Lhoták"
+                        venue="CC '18"
+                        url="https://dl.acm.org/citation.cfm?id=3179499"/>
+
+                    <Paper
+                        title="From Datalog to Flix: A Declarative Language for Fixed Points on Lattices"
+                        authors="Magnus Madsen, Ming-Ho Yee, Ondřej Lhoták"
+                        venue="PLDI '16"
+                        url="https://dl.acm.org/citation.cfm?id=2908096"/>
                 </ul>
 
+                <h3>Workshops</h3>
+
+                <ul>
+                    <Paper
+                        title="Programming a Dataflow Analysis in Flix"
+                        authors="Magnus Madsen, Ming-Ho Yee, Ondřej Lhoták"
+                        venue="TAPAS '16"
+                        url="http://staticanalysis.org/tapas2016/abstracts/TAPAS_2016_MadsenEtAl.pdf"/>
+                </ul>
+
+                <h3>Theses</h3>
+
+                <ul>
+                    <Paper
+                        title="Implementing a Functional Language for Flix"
+                        authors="Ming-Ho Yee"
+                        venue="University of Waterloo"
+                        url="https://uwspace.uwaterloo.ca/bitstream/handle/10012/10856/Yee_Ming-Ho.pdf?sequence=1"
+                    />
+                </ul>
 
             </div>
         );
@@ -34,7 +69,11 @@ class Research extends Component {
 class Paper extends Component {
     render() {
         return (
-            <li>{this.props.name}</li>
+            <li className="mb-3">
+                <a href={this.props.url}>{this.props.title}</a> <b>[{this.props.venue}]</b>
+                <br/>
+                {this.props.authors}
+            </li>
         );
     }
 }
