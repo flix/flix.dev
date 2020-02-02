d0496e40be22d076df043a9db1c384e3d6f747f0
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2020-02-02 16:50:10 +0100

    WIP

--- src/page/blog/Redundancies.js
+++ src/page/blog/Redundancies.js
@@ -19,7 +19,50 @@ class Redundancies extends Component {
                         <h1>Redundancies as Compile-Time Errors</h1>
 
                         <p>
-                            ...
+                            Preamble...
+                        </p>
+
+                        <p>
+                            Consider the following Scala program fragment from the Flix compiler:
+                        </p>
+
+                        <InlineEditor>
+                            {`case Expression.ApplyClo(exp, args, tpe, loc) =>
+    val e = visitExp(exp)
+    val as = args map visitExp
+    Expression.ApplyClo(e, as, tpe, loc)
+
+case Expression.ApplyDef(sym, args, tpe, loc) =>
+    val as = args map visitExp
+    Expression.ApplyDef(sym, as, tpe, loc)
+
+case Expression.Unary(op, exp, tpe, loc) =>
+    val e = visitExp(exp)
+    Expression.Unary(op, exp, tpe, loc)
+
+case Expression.Binary(op, exp1, exp2, tpe, loc) =>
+    val e1 = visitExp(exp1)
+    val e2 = visitExp(exp2)
+    Expression.Binary(op, e1, e2, tpe, loc)`}
+                        </InlineEditor>
+
+                        <p>
+                            Do you spot the issue? If not look closer... OK, got it? The problem is that in the case for
+                            unary expressions, while there is a recursive call on <code>exp</code> with its result
+                            stored in the local variable <code>e</code>, the constructor mistakenly uses
+                            the <code>exp</code> local variable.
+
+                            XXX: This has been in the compiler for a long time. What could have helped? More tests cases, sure.
+                            Or: What about a compiler that warns or gives errors about unused variables? The Scala compiler is mum.
+                        </p>
+
+
+                        <p>
+                            Now
+                        </p>
+
+                        <p>
+                            Flix treats unused and dead-code as compile-time errors.
                         </p>
                     </Col>
                 </Row>
