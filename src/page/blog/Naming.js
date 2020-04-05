01c2ece742564e727b546aac156ef72ac2abe388
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2020-04-05 12:37:25 +0200

    WIP

--- src/page/blog/Naming.js
+++ src/page/blog/Naming.js
@@ -97,9 +97,121 @@ class Naming extends Component {
                             that by an array of strings.
                         </p>
 
+                        <p>
+                            <code>map</code> and <code>mapInPlace</code> is not the only operations where we want to
+                            have both a functional and a destructive operation. In fact, we came up with the following
+                            table:
+
+                        </p>
+
+                        <p>
+                            <table className="table table-striped small">
+                                <thead>
+                                <tr>
+                                    <th scope="col">Functional</th>
+                                    <th scope="col">Destructive</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td>map</td>
+                                    <td>mapInPlace</td>
+                                </tr>
+                                <tr>
+                                    <td>mapWithIndex</td>
+                                    <td>mapWithIndexInPlace</td>
+                                </tr>
+                                <tr>
+                                    <td>reverse</td>
+                                    <td>mapWithIndexInPlace</td>
+                                </tr>
+                                <tr>
+                                    <td>replace</td>
+                                    <td>mapWithIndexInPlace</td>
+                                </tr>
+                                <tr>
+                                    <td>sort</td>
+                                    <td>mapWithIndexInPlace</td>
+                                </tr>
+                                <tr>
+                                    <td>Set.union</td>
+                                    <td>???</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </p>
+
+                        <p>
+                            We then realized we had a general problem and a so a general policy was called for.
+                        </p>
+
+                        <p>
+                            The set union is interesting because ...
+                        </p>
+
+                        <p>
+                            We tried to explore the design space and we came up with the following potential solutions:
+                        </p>
+
+                        <h5>Proposal I: Distinct names</h5>
+
+                        <p>
+                            We can try to come up with distinct names. For example, we might have MutSet.map and
+                            MutSet.transform. Similarly, we might have MutSet.union and MutSet.addAll. The challenge is
+                            to come up with reasonable names. For example, what should we call the two versions of
+                            reverse?
+                        </p>
+
+                        <h5>Proposal II: Prefixed or suffixed-names</h5>
+
+                        <p>
+                            We keep names like map and reverse for the functional variants. For the mutable in place
+                            variants, we prepend or append some specific words, e.g. reverseInPlace or mutReverse.
+                        </p>
+
+                        <h5> Option 3: Prefixed or suffixed-names with symbols </h5>
+
+                        <p>
+                            We go old school. We keep the names map and reverse for the function variants. We name the
+                            mutable variants map! and reverse! using the bang (or some other symbol) to signify
+                            mutation. This seems very principled, but people might really really dislike the aesthetics.
+                        </p>
+
+                        <h5>Option 4: We leverage namespaces</h5>
+
+                        <p>
+                            We use namespaces to disambiguate the operations. For example, we might have Array.reverse
+                            and MutArray.reverse. Problem: For array there is one data structure trying to supports two
+                            types of operations. For Set and MutSet it is more complicated. We would need Set.map,
+                            MutSet.Mut.map, and MutSet.Imm.map. That is, Set is for the pure functional data structure,
+                            MutSet.Mut is for the mutable set where we use mutable map and MutSet.Imm is for the mutable
+                            set but where we use immutable map.
+                        </p>
+
+                        <h5>Option 5: We drop the idea of multiple operations</h5>
+
+                        <p>
+                            If Array and MutSet are mutable then they simply do not come with a functional map or
+                            reverse. If you need that then you must convert the functional to an immutable version and
+                            then call the appropriate operation. For example, a.toImmutable().reverse(). Possibly
+                            toImmutable() (or maybe just toImm) can be implemented more efficiently than building the
+                            entire data structure (but maybe not).
+                        </p>
+
+                        <h5>Option 6: Sort vs. Sorted</h5>
+
+                        <p>
+                            We use words like sort and sorted, reverse and reversed to distinguish between pure and
+                            impure functions.
+                        </p>
 
 
                         <h2>Principles</h2>
+
+                        <p>
+                            We debate these principles, I have already mentioned the discussion below each point, and
+                            ultimately we ended up with the following hybrid solution.
+                        </p>
 
                         <p>
                             This leads us to the principles:
@@ -107,6 +219,11 @@ class Naming extends Component {
 
                         <p>
                             Restate here.
+                        </p>
+
+                        <p>
+                            While we now have some principles, whether they are sufficient is yet to be seen.
+                            The downside of the alieng naming may be off putting.
                         </p>
 
                         <p>
