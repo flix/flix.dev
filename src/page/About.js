1062e46c839cfbe95e1fad4df4b5cfc99549ab84
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2020-05-06 08:27:45 +0200

    WIP

--- src/page/About.js
+++ src/page/About.js
@@ -36,10 +36,6 @@ class About extends Component {
                 </p>
 
                 <p>
-                    Flix is developed by researchers and students at Aarhus University and the University of Waterloo.
-                </p>
-
-                <p>
                     The Flix language and compiler is open source and freely available under the permissive Apache 2.0
                     license.
                 </p>
