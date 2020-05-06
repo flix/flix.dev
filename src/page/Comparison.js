16fee48522c4d276926b866df94cf7f35fcf092f
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2020-05-06 16:14:23 +0200

    WIP

--- src/page/Comparison.js
+++ src/page/Comparison.js
@@ -15,16 +15,23 @@ class Compare extends Component {
 
                 <Row className="mb-3">
                     <Col>
-                        <h1>Compare</h1>
-
-                        Comparing Flix to Scala
+                        <h1>Comparing: Flix versus Scala</h1>
                     </Col>
                 </Row>
 
                 <Row className="mb-3">
 
                     <Col sm={4}>
-                        <h4>Flix Strengths</h4>
+                        <h2>Flix Better</h2>
+
+                        <h5 className="text-primary">Flix Strengths</h5>
+
+                        <ListGroup>
+                            <ListGroupItem>Access to Java Ecosystem</ListGroupItem>
+                            <ListGroupItem>Sound Type System</ListGroupItem>
+                        </ListGroup>
+
+                        <h5 className="text-primary">Scala Weaknesses</h5>
 
                         <ListGroup>
                             <ListGroupItem>Access to Java Ecosystem</ListGroupItem>
@@ -33,15 +40,33 @@ class Compare extends Component {
                     </Col>
 
                     <Col sm={4}>
-                        <h4>Common Strengths</h4>
+                        <h2>Common</h2>
+
+                        <h5>Strengths</h5>
 
                         <ListGroup>
                             <ListGroupItem>Keyword-based Syntax</ListGroupItem>
+                            <ListGroupItem>Local Type Inference</ListGroupItem>
                         </ListGroup>
+
+                        <h5>Weaknesses</h5>
+                        <ListGroup>
+                            <ListGroupItem>Keyword-based Syntax</ListGroupItem>
+                            <ListGroupItem>Local Type Inference</ListGroupItem>
+                        </ListGroup>
+
                     </Col>
 
                     <Col sm={4}>
-                        <h4>Scala Weaknesses</h4>
+                        <h2>Scala Better</h2>
+
+                        <h5 className="text-primary">Scala Strengths</h5>
+
+                        <ListGroup>
+                            <ListGroupItem>Null</ListGroupItem>
+                        </ListGroup>
+
+                        <h5 className="text-primary">Flix Weaknesses</h5>
 
                         <ListGroup>
                             <ListGroupItem>Null</ListGroupItem>
