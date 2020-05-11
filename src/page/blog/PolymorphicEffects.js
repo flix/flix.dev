ccc6899a21abf8a8e5140a26439d18b736e24997
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2020-05-11 13:56:10 +0200

    WIP

--- src/page/blog/PolymorphicEffects.js
+++ src/page/blog/PolymorphicEffects.js
@@ -1,8 +1,7 @@
 import React, {Component} from "react";
 import ReactGA from "react-ga";
-import {Card, CardText, CardTitle, Col, Container, Row} from "reactstrap";
+import {Col, Container, Row} from "reactstrap";
 import InlineEditor from "../../util/InlineEditor";
-import {Link} from "react-router-dom";
 
 class PolymorphicEffects extends Component {
 
@@ -23,7 +22,64 @@ class PolymorphicEffects extends Component {
                             Posted May 2020 by <i>Magnus Madsen</i>.
                         </p>
 
+                        <InlineEditor>
+                            {`def exists(f: a -> Bool, xs: Set[a]): Bool & Pure `}
+                        </InlineEditor>
 
+
+                        <InlineEditor>
+                            {`def foreach(f: a ~> Unit, xs: List[a]): Unit & Impure = `}
+                        </InlineEditor>
+
+                        <InlineEditor>
+                            {`def onMouseDown(f: MouseEvent ~> Unit): Unit & Impure
+                            def onMouseUp(f: MouseEvent ~> Unit): Unit & Impure = ...`}
+                        </InlineEditor>
+
+                        <InlineEditor>
+                            {`def assert(f: Unit -> Bool): Unit & Pure = ...
+                            def log(f: Unit -> String , l: LogLevel): Unit & Pure = ...`}
+                        </InlineEditor>
+
+                        <InlineEditor>
+                            {`1 trait Eq[a] {
+2 def eq(x: a, y: a): Bool & Pure
+3 }`}
+                        </InlineEditor>
+
+                        <InlineEditor>
+                            {` def minBy(f: a -> b, l: List[a]): a = ...
+2 def maxBy(f: a -> b, l: List[a]): a = ...
+3 def sortBy(f: a -> Int32, l: List[a]): List[a] = ...
+4 def groupBy(f: a -> k, l: List[a]): Map[k, List[a]] = ...`}
+                        </InlineEditor>
+
+                        <InlineEditor>
+                            {`spawn (2 + 2`}
+                        </InlineEditor>
+
+                        <InlineEditor>
+                            {`def unfoldWithIter(next: Unit ~> Option[a]): List[a] & Impure`}
+                        </InlineEditor>
+
+
+                        <InlineEditor>
+                            {`1 def main(): Int =
+2 List.map(x -> x + 1, 1 :: 2 :: Nil);
+3 123`}
+                        </InlineEditor>
+
+
+                        <InlineEditor>
+                            {`1 -- Redundancy Error ------------------ foo.flix
+2
+3 >> Useless expression: It has no side-effect(s)
+4 and its result is discarded.
+5
+6 2 | List.map(x -> x + 1, 1 :: 2 :: Nil);
+7 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+8 useless expression.`}
+                        </InlineEditor>
 
                     </Col>
                 </Row>
