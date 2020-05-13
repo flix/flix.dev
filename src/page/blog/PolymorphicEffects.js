e1dfcbd260d6d0120e3bda5184eb7afd9cd633c6
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2020-05-13 21:33:15 +0200

    WIP

--- src/page/blog/PolymorphicEffects.js
+++ src/page/blog/PolymorphicEffects.js
@@ -537,16 +537,17 @@ def stripIndentHelper(n: Int32, s: String): String & Impure =
                         <h2>Type Inference and Boolean Unification</h2>
 
                         <p>
-                            The Flix type and effect system supports type inference. That is, explicit type annotations
-                            are never required (although as a design choice we demand such annotations for all top-level
-                            definitions). The benefit is that, within a function, the programmer never has to worry
-                            about pure and impure expressions. The compiler automatically infers whether every
-                            expression is pure, impure, or effect polymorphic.
+                            The Flix type and effect system supports inference. Explicit type annotations are never
+                            required locally within a function. As a design choice, we do require type signatures for
+                            top-level definitions. Within a function, the programmer never has to worry about pure and
+                            impure expressions; the compiler automatically infers whether an expression is pure, impure,
+                            or effect polymorphic. The programmer only has to ensure that the declared type and effect
+                            matches the type and effect of the function body.
                         </p>
 
                         <p>
-                            The details of the type and effect system are the subject of a research paper (currently in
-                            submission). The paper will be made available here when accepted for publication.
+                            The details of the type and effect system are the subject of a forthcoming research paper
+                            and will be made available in due time.
                         </p>
 
                         <h2>Closing Thoughts</h2>
