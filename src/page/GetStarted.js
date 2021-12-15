3247901038e6de1b8b7ee4d2200314586b772835
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2021-12-15 11:36:09 +0100

    feat: add section on manually downloading and running the compiler.

--- src/page/GetStarted.js
+++ src/page/GetStarted.js
@@ -13,30 +13,51 @@ class GetStarted extends Component {
     render() {
         return (
             <Container>
-                <h1>Getting Started</h1>
+                <h1>Get Started</h1>
 
                 <Row className="mb-4">
                     <Col>
-                        <h5>Online Playground</h5>
+                        <h5>Using the Online Playground</h5>
                         <p>
-                            You can try Flix online in the playground at <a href="https://play.flix.dev/">play.flix.dev</a>.
+                            You can try Flix online in the playground at <a
+                            href="https://play.flix.dev/">play.flix.dev</a>.
                         </p>
                     </Col>
                 </Row>
 
-                <Row>
+                <Row className="mb-4">
                     <Col>
-                        <h5>Visual Studio Code</h5>
+                        <h5>Using Visual Studio Code</h5>
 
                         <p>
-                            Or you can easily install Flix in <a href="https://code.visualstudio.com/">Visual Studio Code</a>:
+                            Or you can easily install Flix in <a href="https://code.visualstudio.com/">Visual Studio
+                            Code</a>:
                         </p>
 
-                        <Card className="ml-2 mr-2">
+                        <Card className="ml-5 mr-5">
                             <CardImg top src={VSCode}/>
                         </Card>
                     </Col>
                 </Row>
+
+                <Row className="mb-4">
+                    <Col>
+                        <h5>Manually downloading and running the compiler</h5>
+                        <p>
+                            <ol>
+                                <li>Ensure that you have at least Java 11 installed. You can check with <code>java -version</code>.</li>
+                                <li>Download the latest version of the Flix compiler (<code>flix.jar</code>) at <a
+                                    href="https://github.com/flix/flix/releases/latest">https://github.com/flix/flix/releases/latest</a>.
+                                </li>
+                                <li>Create an empty folder (e.g. <code>mkdir flixproject</code>) and place the downloaded Flix JAR (<code>flix.jar</code>) into that folder.</li>
+                                <li>Enter the created directory (e.g. <code>cd flixproject</code>) and run <code>java -jar flix.jar init</code> to create an empty Flix project.</li>
+                                <li>Run <code>java -jar flix.jar run</code> to compile and run the project.</li>
+                                <li>Flix will compile the project and execute the <code>main</code> function (located in <code>src/Main.flix</code>).</li>
+                            </ol>
+                        </p>
+                    </Col>
+                </Row>
+
 
             </Container>
         );
