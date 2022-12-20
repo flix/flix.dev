91a8fc47de9b1c7a1bbae8e15ea5d26e0baab39b
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2022-12-20 14:06:16 +0100

    chore: update `Internships.js`

--- src/page/Internships.js
+++ src/page/Internships.js
@@ -9,49 +9,54 @@ class Internships extends Component {
 
     render() {
         return (<Container>
-                <Row className="mb-3">
-                    <Col>
+            <Row className="mb-3">
+                <Col>
 
-                        <h1>Research Internships</h1>
+                    <h1>Research Internships</h1>
 
-                        <p className="font-weight-bold mt-4 mb-4">
-                            Interested in programming language research?
-                            Want to work on compilers?
-                            Want to work on Flix?
-                        </p>
+                    <p className="font-weight-bold mt-4 mb-4">
+                        Interested in programming language research?
+                        Want to work on compilers?
+                        Want to work on Flix?
+                    </p>
 
-                        <p>
-                            Then join us for a research internship at Aarhus University in Denmark!
-                        </p>
+                    <p>
+                        Then join us for a research internship at Aarhus University in Denmark!
+                    </p>
 
-                        <hr/>
+                    <hr/>
 
-                        <p>
-                            To apply, send an email to <code>magnusm at cs dot au dot dk</code> with a bit of
-                            information about yourself.
-                        </p>
+                    <p>
+                        To apply, send an email to <code>magnusm at cs dot au dot dk</code> with a bit of
+                        information about yourself.
+                    </p>
 
-                        <p>
-                            A typical research internship lasts 6-8 weeks in the summer period (but other times of year
-                            may also be possible).
-                        </p>
+                    <p>
+                        A typical research internship lasts 6-8 weeks in the summer period (but other times of year
+                        may also be possible).
+                    </p>
 
-                        <hr/>
+                    <hr/>
 
-                        <p>
-                            Aarhus University will reimburse travel and visa costs for coming to Denmark.
-                            In addition, it is possible to apply for a small stipend to help cover housing and living
-                            expenses. To apply, you must currently be enrolled in a bachelor's or master's degree
-                            programme at an accredited university.
-                        </p>
+                    <p>
+                        Aarhus University will reimburse travel and visa costs for coming to Denmark.
+                        In addition, it is possible to apply for a small stipend to help cover housing and living
+                        expenses.
+                    </p>
 
-                        <p>
-                            If you have questions, feel free to head over to <a
-                            href="https://gitter.im/flix/Lobby">Gitter</a> and talk to us.
-                        </p>
-                    </Col>
-                </Row>
-            </Container>);
+                    <p>
+                        To apply, you should be enrolled in a bachelor's or master's degree programme at an accredited
+                        university. You should have taken one or two courses on programming languages and on compilers
+                        (exceptions can be made for especially talented students.)
+                    </p>
+
+                    <p>
+                        If you have questions, feel free to head over to <a
+                        href="https://gitter.im/flix/Lobby">Gitter</a> and talk to us.
+                    </p>
+                </Col>
+            </Row>
+        </Container>);
     }
 }
 
