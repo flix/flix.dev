22c27d5bdad24b159bbe27fdec8b2ff0bae2c57d
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2023-12-23 00:41:37 +0100

    feat: update `Documentation` page (#125)

--- src/page/Documentation.js
+++ src/page/Documentation.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
-import { Card, CardSubtitle, CardText, CardTitle, Col, Container, Row } from "reactstrap";
-import { Link } from "react-router-dom";
+import React, {Component} from 'react';
+import {Card, CardSubtitle, CardText, CardTitle, Col, Container, Row} from "reactstrap";
+import {Link} from "react-router-dom";
 
-import { FaBookReader } from 'react-icons/fa';
-import { FaStream } from 'react-icons/fa';
-import { FaMicroblog } from 'react-icons/fa';
-import { FaGitter } from 'react-icons/fa';
-import { FaGithub } from 'react-icons/fa';
-import { GoGraph } from 'react-icons/go';
+import {FaBookReader} from 'react-icons/fa';
+import {FaStream} from 'react-icons/fa';
+import {FaMicroblog} from 'react-icons/fa';
+import {FaGitter} from 'react-icons/fa';
+import {FaGithub} from 'react-icons/fa';
+import {FaTwitter} from 'react-icons/fa';
+import {GoGraph} from 'react-icons/go';
+import {MdSchool} from 'react-icons/md';
 
 class Documentation extends Component {
 
@@ -26,35 +28,38 @@ class Documentation extends Component {
 
                 <Row className="mb-4">
                     <Col md="6">
-                        <a href="https://doc.flix.dev/" className="text-primary">
-                            <Card body className="h-100">
-                                <CardSubtitle className="text-center m-4">
-                                    <FaBookReader style={{ fontSize: '6em' }} />
-                                </CardSubtitle>
-                                <CardTitle className="text-center">
-                                    Programming Flix
-                                </CardTitle>
-                                <CardText className="text-dark">
-                                    The book provides an introduction to Flix for functional programmers. The book
-                                    demonstrates the core concepts of Flix through several examples.
-                                </CardText>
-                            </Card>
-                        </a>
+                        <Card body className="h-100">
+                            <CardSubtitle className="text-center m-4">
+                                <a href="https://doc.flix.dev/" className="text-primary">
+                                    <FaBookReader style={{fontSize: '6em'}}/>
+                                </a>
+                            </CardSubtitle>
+                            <CardTitle className="text-center">
+                                <a href="https://doc.flix.dev/" className="text-primary">
+                                    Programming Flix (doc.flix.dev)
+                                </a>
+                            </CardTitle>
+                            <CardText className="text-dark text-center">
+                                A detailed introduction to the Flix Programming Language.
+                            </CardText>
+                        </Card>
                     </Col>
                     <Col md="6">
-                        <a href="https://api.flix.dev/" className="text-success">
-                            <Card body className="h-100">
-                                <CardSubtitle className="text-center m-4">
-                                    <FaStream style={{ fontSize: '6em' }} />
-                                </CardSubtitle>
-                                <CardTitle className="text-center">
-                                    Standard Library
-                                </CardTitle>
-                                <CardText className="text-dark">
-                                    The documentation provides a Javadoc-style description of the Flix library.
-                                </CardText>
-                            </Card>
-                        </a>
+                        <Card body className="h-100">
+                            <CardSubtitle className="text-center m-4">
+                                <a href="https://api.flix.dev/" className="text-success">
+                                    <FaStream style={{fontSize: '6em'}}/>
+                                </a>
+                            </CardSubtitle>
+                            <CardTitle className="text-center">
+                                <a href="https://api.flix.dev/" className="text-success">
+                                    Standard Library (api.flix.dev)
+                                </a>
+                            </CardTitle>
+                            <CardText className="text-dark text-center">
+                                A Javadoc-style description of the Flix Standard Library.
+                            </CardText>
+                        </Card>
                     </Col>
                 </Row>
 
@@ -68,10 +73,23 @@ class Documentation extends Component {
 
                 <Row className="mb-3">
                     <Col lg="2">
+                        <a href="https://doc.flix.dev/research-literature.html">
+                            <Card body className="h-100">
+                                <CardSubtitle className="text-center m-4 text-black-50">
+                                    <MdSchool style={{fontSize: '3em'}}/>
+                                </CardSubtitle>
+                                <CardTitle className="text-center link-primary">
+                                    Research
+                                </CardTitle>
+                            </Card>
+                        </a>
+                    </Col>
+
+                    <Col lg="2">
                         <Link to="/blog/">
                             <Card body className="h-100">
                                 <CardSubtitle className="text-center m-4 text-black-50">
-                                    <FaMicroblog style={{ fontSize: '3em' }} />
+                                    <FaMicroblog style={{fontSize: '3em'}}/>
                                 </CardSubtitle>
                                 <CardTitle className="text-center link-primary">
                                     Blog
@@ -84,7 +102,7 @@ class Documentation extends Component {
                         <a href="https://gitter.im/flix/Lobby">
                             <Card body className="h-100">
                                 <CardSubtitle className="text-center m-4 text-black-50">
-                                    <FaGitter style={{ fontSize: '3em' }} />
+                                    <FaGitter style={{fontSize: '3em'}}/>
                                 </CardSubtitle>
                                 <CardTitle className="text-center link-primary">
                                     Gitter
@@ -97,7 +115,7 @@ class Documentation extends Component {
                         <a href="https://github.com/flix/flix">
                             <Card body className="h-100">
                                 <CardSubtitle className="text-center m-4 text-black-50">
-                                    <FaGithub style={{ fontSize: '3em' }} />
+                                    <FaGithub style={{fontSize: '3em'}}/>
                                 </CardSubtitle>
                                 <CardTitle className="text-center link-primary">
                                     GitHub
@@ -107,13 +125,26 @@ class Documentation extends Component {
                     </Col>
 
                     <Col lg="2">
+                        <a href="https://twitter.com/flixlang">
+                            <Card body className="h-100">
+                                <CardSubtitle className="text-center m-4 text-black-50">
+                                    <FaTwitter style={{fontSize: '3em'}}/>
+                                </CardSubtitle>
+                                <CardTitle className="text-center link-primary">
+                                    Twitter
+                                </CardTitle>
+                            </Card>
+                        </a>
+                    </Col>
+
+                    <Col lg="2">
                         <a href="https://arewefast.flix.dev/">
                             <Card body className="h-100">
                                 <CardSubtitle className="text-center m-4 text-black-50">
-                                    <GoGraph style={{ fontSize: '3em' }} />
+                                    <GoGraph style={{fontSize: '3em'}}/>
                                 </CardSubtitle>
                                 <CardTitle className="text-center link-primary">
-                                    Compiler Perf
+                                    Perf
                                 </CardTitle>
                             </Card>
                         </a>
